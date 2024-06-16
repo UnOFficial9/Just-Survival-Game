e08e77d1218fb0d2a5431ad047c2f281807fcdda
Artem Garder <tyoma.garder@gmail.com>
2024-06-16 21:09:39 +0200

    added more volume option and modified bullets

--- Assets/Scripts/Player, Gun/Bullet.cs
+++ Assets/Scripts/Player, Gun/Bullet.cs
@@ -7,6 +7,7 @@ public class Bullet : MonoBehaviour
     public float speed;
     public int lifeTime;
     public int damage;
+    public AudioClip[] clips = new AudioClip[3];
     private void Start()
     {
         Destroy(gameObject, lifeTime);
@@ -15,5 +16,11 @@ public class Bullet : MonoBehaviour
     {
         transform.Translate(new Vector3 (0, 1, 0) * speed * Time.deltaTime);
 
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<AudioSource>().clip = clips[Random.Range(0, clips.Length)];
+        GetComponent<AudioSource>().Play();
+        Destroy(gameObject);
     }
 }
